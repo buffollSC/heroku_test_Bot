8ddf0218c2d36f446d5375b4200a5ae5c541987c
Roman <buffollSC@gmail.com>
2019-12-23 17:23:06 +0300

    noob

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const myToken = '995308759:AAG0cSOOdlAP8r3n6tnaXtBx0wArse89YDA'
 const library = require('node-telegram-bot-api')
 const bot = new library(myToken,{polling: true})
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from HEROKU,bot says:"Hi, ${msg.from.first_name}"')
+    bot.sendMessage(msg.chat.id, 'Stas is NOOB')
 })
